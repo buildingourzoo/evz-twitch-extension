5a5c34640e10a12b11d09f64139bd443148265f7
buildingourzoo <marketing@buildingourzoo.com>
2026-04-14 15:31:25 -0600

    Update twitch.js

--- js/twitch.js
+++ js/twitch.js
@@ -1,1 +1,13 @@
 console.log("Twitch JS loaded");
+
+if (window.Twitch && Twitch.ext) {
+  Twitch.ext.onAuthorized(function(auth) {
+    console.log("Twitch authorized:", auth);
+  });
+
+  Twitch.ext.onContext(function(context) {
+    console.log("Twitch context:", context);
+  });
+} else {
+  console.log("Twitch.ext not available yet");
+}
